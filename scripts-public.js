0981047b231bb6711941b005dbf4ff434450a8b6
Son2x Judan <son2x.judan@gmail.com>
2019-08-02 15:40:14 +0800

    assets update
    assets

---
+++ scripts-public.js
@@ -0,0 +1,18 @@
+$('.carousel-portfolio').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+$('.carousel-reviews').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
